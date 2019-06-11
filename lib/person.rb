e9ff82ac4eb3ae905d29851079783fe27b783250
John Jordan Shelley <jshelley553@gmail.com>
2019-06-11 04:25:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,8 +8,21 @@ class Person
          @name
        end
 
+       def job=(with_jobs)
+         @job = with_jobs
+       end
+
+       def job
+         @job
+       end
+
 end
 
   beyonce = Person.new
   beyonce.name = "Beyonce"
   puts beyonce.name
+
+
+beyonce = Person.new
+beyonce.job = "Singer"
+puts beyonce.job
