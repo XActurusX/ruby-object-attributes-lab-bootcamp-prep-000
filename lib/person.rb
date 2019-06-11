d8390280e40570928b8d30e76745e1f284b978e6
John Jordan Shelley <jshelley553@gmail.com>
2019-06-11 04:20:32 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+       def name=(with_names)
+         @name = with_names
+       end
+
+
+       def name
+         @name
+       end 
+
+end
+
+  beyonce.Person.new
+  beyonce.name = "Beyonce"
+  puts beyonce.name
